d39f5aa8180b4869c598987f0c51eb9320df130a
Erran Carey <me@errancarey.com>
2019-03-23 19:49:49 +0000

    Use lua-resty-hmac to generate correct signature

--- README.md
+++ README.md
@@ -3,19 +3,14 @@ A [Kong][] plugin for [verifying requests from Slack][].
 
 ## Development
 The following can be used to setup your environment on Mac:
-
 ```sh
-brew install lua
-brew install luarocks
-brew install openssl
+brew install openresty
 ```
 
 Lua dependencies:
 ```
 luarocks-5.1 install busted
-luarocks-5.1 install lua-cjson
-luarocks-5.1 install luaossl CRYPTO_INCDIR=/usr/local/opt/openssl/include/ OPENSSL_DIR=/usr/local/opt/openssl/
-luarocks-5.1 install lbase64
+#luarocks-5.1 install luaossl CRYPTO_INCDIR=/usr/local/opt/openssl/include/ OPENSSL_DIR=/usr/local/opt/openssl/
 ```
 
 [Kong]: https://konghq.com/kong/
--- src/kong/plugins/slack-hmac/signer.lua
+++ src/kong/plugins/slack-hmac/signer.lua
@@ -1,16 +1,9 @@
-local openssl_hmac = require("openssl.hmac")
-local sha256 = require("resty.sha256")
+local resty_hmac = require("resty.hmac")
 local to_hex = require("resty.string").to_hex
 
-local encode_base64 = ngx.encode_base64
+local decode_base64 = ngx.decode_base64
 
 local Signer = {}
-
-function Signer:hexdigest256(content)
-  local digest = sha256:new()
-  digest:update(content)
-  return to_hex(digest:final())
-end
 
 -- request table:
 --   body: The raw Slack request body.
@@ -18,8 +11,12 @@ end
 --   version: The Slack API version such as v0.
 function Signer:sign(request, secret)
   local payload = 'v0:' .. request.timestamp .. ':' .. request.body
-  local hmac_signature = openssl_hmac.new(secret, "sha256"):final(payload)
-  return 'v0=' .. Signer:hexdigest256(hmac_signature)
+  local hmac_signature, err = resty_hmac:new(secret):generate_signature("sha256", payload)
+  if err then
+    return nil, err
+  end
+
+  return 'v0=' .. to_hex(decode_base64(hmac_signature)), nil
 end
 
 return Signer
